a824e4ff479409971c8157f2bf3af5b7f1467c0f
Dusty <dustymccord@gmail.com>
2019-12-30 22:55:43 -0800

    writing to firestore.

--- public/firebase.js
+++ public/firebase.js
@@ -1,20 +1,20 @@
-// var app_fireBase = {};
+// Initialize the FirebaseUI Widget using Firebase.
+// var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-// (function(){
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyBclxMSOj2cGxnWDXLSV9-a0Agu_YmpWPM",
-    authDomain: "kparty-38f4c.firebaseapp.com",
-    databaseURL: "https://kparty-38f4c.firebaseio.com",
-    projectId: "kparty-38f4c",
-    storageBucket: "kparty-38f4c.appspot.com",
-    messagingSenderId: "619007555753",
-    appId: "1:619007555753:web:f20c6f32e1942512ec11b8",
-    measurementId: "G-40HY6HS3RY"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-  
-//   app_fireBase = firebase;
-// })();
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBclxMSOj2cGxnWDXLSV9-a0Agu_YmpWPM",
+  authDomain: "kparty-38f4c.firebaseapp.com",
+  databaseURL: "https://kparty-38f4c.firebaseio.com",
+  projectId: "kparty-38f4c",
+  storageBucket: "kparty-38f4c.appspot.com",
+  messagingSenderId: "619007555753",
+  appId: "1:619007555753:web:f20c6f32e1942512ec11b8",
+  measurementId: "G-40HY6HS3RY"
+};
+
+// Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+var firestore = firebase.firestore();
--- public/main.js
+++ public/main.js
@@ -1,17 +1,51 @@
 
-var uid = null;
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        uid = user.uid;
-        // User is signed in.
-    }else{
-        uid = null;
-        window.location.replace('index.html');
-    }
-});
+var isLoggedIn = function(){
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+        }else{
+            window.location.replace('index.html');
+        }
+    });
+  
+};
+isLoggedIn();
 
 function logOut (){
     firebase.auth().signOut();
 }
+
+
+// ------- NEW BUTTON LISTEN ------
+var btnNew = document.getElementById('new');
+btnNew.addEventListener('click', function(){
+
+    var userID = firebase.auth().currentUser.uid;
+
+    var endOfYear = {
+        name: 'end of year',
+        user: userID,
+        songs: 'all the hits'
+    }
+
+    firestore.collection("parties").add(endOfYear)
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+});
+
+
+// ------- SHOW BUTTON LISTEN ------
+var btnShow = document.getElementById('show');
+    btnShow.addEventListener('click', function(){
+    document.getElementById('test').innerHTML = firebase.auth().currentUser.uid;
+    
+});
+
+
+
 
 
