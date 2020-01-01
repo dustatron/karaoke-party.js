46fb3aea19af0ca5d87af4b748ff56650fa3b1b4
Dusty <dustymccord@gmail.com>
2020-01-01 01:01:13 -0800

    cleaned up global name space.

--- public/main.js
+++ public/main.js
@@ -1,52 +1,59 @@
 var partyDB = firestore.collection("parties");
-var userID;
+
 
 //---- Check Log in Status ------
 var isLoggedIn = function(){
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             // User is signed in.
-            userID = firebase.auth().currentUser.uid;
-            LiveUpdate();
+            var userID = firebase.auth().currentUser.uid;
+            LiveUpdate(userID);
         }else{
             window.location.replace('index.html');
         }
     });
   
 }();
-// isLoggedIn();
-
-function logOut (){
-    firebase.auth().signOut();
-}
 
 
 // ------- New Party Button ------
-var btnNew = document.getElementById('new');
-var inputNew = document.getElementById('newInput');
-btnNew.addEventListener('click', function(){
+var newParty = function() {
+    var btnNew = document.getElementById('new');
 
-    var userID = firebase.auth().currentUser.uid;
+    var inputNew = document.getElementById('newInput');
+    btnNew.addEventListener('click', function(){
+        var user = firebase.auth().currentUser;
+        var currentTime = new Date().getTime();
 
-    var newParty = {
-        name: newInput.value,
-        user: userID
-    }
+        //New Party Object
+        var newParty = {
+            partyName: newInput.value,
+            timestamp: currentTime,
+            user: user.uid,
+            userName: user.displayName,
+            userEmail: user.email,
+            userImage: user.photoURL
+        }
 
-    firestore.collection("parties").add(newParty)
-    .then(function(docRef) {
-        // drawParties();
-        console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function(error) {
-        console.error("Error adding document: ", error);
+        //Write to Firestore
+        firestore.collection("parties").add(newParty)
+        .then(function(docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function(error) {
+            console.error("Error adding document: ", error);
+        });
     });
-});
+}();
 
 
 //---- App Logic ----
 
-var killParty = function(partyID){
+var logOut = function() {
+    firebase.auth().signOut();
+}
+
+var killParty = function(partyID) {
     partyDB.doc(partyID).delete().then(function() {
         console.log("Document successfully deleted!");
     }).catch(function(error) {
@@ -55,7 +62,7 @@ var killParty = function(partyID){
 };
 
 
-var LiveUpdate = function() {
+var LiveUpdate = function(userID) {
     partyDB.where('user', "==", userID)
         .onSnapshot(function(querySnapshot) {
             document.getElementById('list').innerHTML = " ";
@@ -66,7 +73,7 @@ var LiveUpdate = function() {
                 // Draws Party box
                 document.getElementById('list').innerHTML += "<div id='"
                 + id +"-01' class='card' > Party: "
-                + doc.data().name +
+                + doc.data().partyName +
                 '<button class="btn btn-primary">Add Songs</button><button id='
                 + id +' class="delete-btn btn btn-danger" >Delete</button></div>';
 
